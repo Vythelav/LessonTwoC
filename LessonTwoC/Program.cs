d59c84b417662699378098520529d3e97f664238
Vythelav <slava.gusaov@gmail.com>
2024-04-23 10:42:07 +0300

    Добавьте файлы проекта.

---
+++ LessonTwoC/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+//1
+
+
+//Console.Write("Simple input: ");
+//string name = Console.ReadLine();
+//Console.WriteLine($"Simple output: \n Hello \n {name}!");
+
+
+//2
+
+
+//Console.Write("Введите число А: ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите число Б ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Сумма: " + (a + b));
+//Console.WriteLine();
+//Console.WriteLine("Разность: " + (a - b));
+//Console.WriteLine();
+//Console.WriteLine("Произведение: " + (a * b));
+//Console.WriteLine();
+//Console.WriteLine("Частное: " + (a / b));
+
+
+//3
+//int a = 6;
+//int b = 7;
+
+//string s = a.ToString();
+//string f = b.ToString();
+
+//Console.WriteLine(s + " " + f);
+
+
+//4
+
+//string a = "6";
+//string b = "7";
+
+
+//float s = float.Parse(a);
+//float f = float.Parse(b);
+
+//Console.WriteLine(s + " " + f);
+
+
+//5
+
+
+//Console.Write("Введите градсы Цульсия: ");
+//double t = Convert.ToDouble(Console.ReadLine());
+
+//double f = (t * 9 / 5) + 32;
+//Console.WriteLine("Градусы Фаренгейта: " + f);
+
+
+//6
+
+//Console.Write("Введите количество секунд: ");
+//int totalSeconds = Convert.ToInt32(Console.ReadLine());
+
+//int hours = totalSeconds / 60;
+//int minutes = (totalSeconds % 3600) / 60;
+//int seconds = totalSeconds % 60;
+
+//hours = hours % 24;
+
+//Console.WriteLine($"Текущее время: {hours} часов {minutes} минут {seconds} секунд");
+
+
+
+
+
